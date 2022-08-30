243d1db362b92d7fc70d09e21e9e6635255af2ca
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-08-30 18:27:13 -0300

    Second commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*
+El usuario debe poder seleccionar y enviar un valor numérico.
+Luego se imprime en pantalla el mensaje de agradecimiento.
+
+HTML para imprimir en el DOM:
+<!-- Thank you state start -->
+    <div>
+        <img src="./images/illustration-thank-you.svg" alt="Thank you">
+    </div>
+
+    <div>
+        <p>You selected <!-- Add rating here --> out of 5</p>
+    </div>
+
+    <div>
+        <h2>Thank you!</h2>
+    </div>
+
+    <div>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+    </div>
+<!-- Thank you state end -->
+
+
+*/
