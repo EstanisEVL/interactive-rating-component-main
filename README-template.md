b91bf5e251c34f31d1db66c53a184698e70b7946
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-09-04 17:17:40 -0300

    Fourth commit

--- README-template.md
+++ README-template.md
@@ -14,7 +14,6 @@ This is a solution to the [Interactive rating component challenge on Frontend Me
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
 
 ## Overview
 
@@ -29,15 +28,9 @@ Users should be able to:
 
 ### Screenshot
 
-![](./screenshot.jpg)
+- Desktop preview: ![Desktop](./Desktop-Preview-Frontend-Mentor-Interactive-rating-component.png)
 
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+- Mobile preview: ![Mobile](./Mobile-Preview-Frontend-Mentor-Interactive-rating-component.png)
 
 ### Links
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Save rating:
 let ratingBtn = document.querySelectorAll(".btn--rating");
-
 ratingBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
         let rating = btn.textContent;
@@ -8,32 +7,15 @@ ratingBtn.forEach((btn) => {
     })
 });
 
-// Print message:
-const printMessage = () => {
-    const divContainer = document.getElementById("main--container");
-    let retrieveRating = JSON.parse(localStorage.getItem("rating"));
-
-    const div = document.createElement("div");
-    div.classList.add("div--container__thanks");
-    div.innerHTML = `
-                    <div class="div--img__thanks">
-                        <img src="./images/illustration-thank-you.svg" alt="Thank you">
-                    </div>
-                    <div class="div--rating__thanks">
-                        <p>You selected ${retrieveRating} out of 5</p>
-                    </div>
-                    <div class="div--title__thanks">
-                        <h2>Thank you!</h2>
-                    </div>
-                    <div class="div--paragraph__thanks">
-                        <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-                    </div>
-                    `
-
-    divContainer.innerHTML = "";
-    divContainer.appendChild(div);
-}
-const submitBtn = document.getElementById("submit");
+// Show thank you message and rating selected:
+let submitBtn = document.getElementById("submit");
 submitBtn.addEventListener("click", () => {
-    printMessage();
-});
+    ratingContainer.classList.add("hide");
+    thanksContainer.classList.remove("hide");
+
+    rate.textContent = localStorage.getItem("rating");
+})
+
+const ratingContainer = document.querySelector(".div--container");
+const thanksContainer = document.querySelector(".div--container__thanks");
+const rate = document.getElementById("rating");
