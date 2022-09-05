9b913b8d9203ed933c0ed7382b7e148e3626ea91
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-09-05 16:50:55 -0300

    Fifth commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
-// Save rating:
-let ratingBtn = document.querySelectorAll(".btn--rating");
-ratingBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        let rating = btn.textContent;
-        let saveRating = localStorage.setItem("rating", rating);
+(() => {
+    // Variables:
+    let ratingBtn = document.querySelectorAll(".btn--rating");
+    const ratingContainer = document.querySelector(".div--container");
+    const thanksContainer = document.querySelector(".div--container__thanks");
+    const rate = document.getElementById("rating");
+    let submitBtn = document.getElementById("submit");
+
+    // Save rating:
+    ratingBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            let rating = btn.textContent;
+            let saveRating = localStorage.setItem("rating", rating);
+        })
+    });
+
+    // Show thank you message and rating selected:
+    submitBtn.addEventListener("click", () => {
+        ratingContainer.classList.add("hide");
+        thanksContainer.classList.remove("hide");
+    
+        rate.textContent = localStorage.getItem("rating");
     })
-});
-
-// Show thank you message and rating selected:
-let submitBtn = document.getElementById("submit");
-submitBtn.addEventListener("click", () => {
-    ratingContainer.classList.add("hide");
-    thanksContainer.classList.remove("hide");
-
-    rate.textContent = localStorage.getItem("rating");
-})
-
-const ratingContainer = document.querySelector(".div--container");
-const thanksContainer = document.querySelector(".div--container__thanks");
-const rate = document.getElementById("rating");
+})();
